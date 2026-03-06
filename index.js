b85ef3582aa859a50f45fe2a8fb664f54996943d
unknown <taiburrahamanapu@gmail.com>
2026-03-06 11:59:20 +0600

    added products routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 const app = express()
@@ -9,6 +10,52 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
+const uri = process.env.DB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+   const database = client.db('yassoraaDB')
+   const productsCollection = database.collection('products')
+    // Send a ping to confirm a successful connection
+
+    app.post('/products',async(req, res)=>{
+        const product = req.body
+    const result = await productsCollection.insertOne(product)
+res.send(result )})
+
+app.get('/products', async( req, res)=>{
+    const cursor = productsCollection.find()
+    const products = await cursor.toArray()
+    res.send(products)
+})
+
+app.get('/products/:id', async(req, res)=>{
+    const id = req.params.id
+    const query ={_id: new ObjectId(id)}
+    const product = await productsCollection.findOne(query)
+    res.send(product)})
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res)=>{
     res.send('Hello World!')
 })
@@ -16,3 +63,4 @@ app.get('/', (req, res)=>{
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
+
